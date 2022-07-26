ac0dec80189fbcb4bad0da9358f3825d24ead36e
Jana Ahurtsova <byjankaaa@gmail.com>
2022-07-26 12:17:47 +0300

    feat: add solving to task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) return [];
+
+  return matrix.reduce((acc, el, ind) => {
+    if(ind % 2 === 0) {
+      acc.push(...el);
+    } else {
+      acc.push(...el.reverse());
+    }
+    return acc;
+  }, [])
 }
